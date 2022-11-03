a9a2e8c8e85e48c4268042361a706bb89a8874f6
unknown <arthurguilherme2013@gmail.com>
2022-11-03 07:27:30 +0000

    Revendo Strings e Boolean

---
+++ 2_TiposDeDados/6_Strings/js/script.js
@@ -0,0 +1,5 @@
+console.log(typeof "Hello my brother!");
+console.log(typeof 'I need to study more!');
+console.log(typeof `Teste Template Literals`);
+console.log(typeof `Infinity`);
+console.log(typeof Infinity);
---
+++ 2_TiposDeDados/7_String_Avancado/js/script.js
@@ -0,0 +1,4 @@
+console.log("Primeira linha \nSegunda linha");
+console.log("My names 'Arthur'");
+console.log('My names "Arthur"');
+console.log(`A multiplicação de 5 por três é: ${5 * 3}`);
