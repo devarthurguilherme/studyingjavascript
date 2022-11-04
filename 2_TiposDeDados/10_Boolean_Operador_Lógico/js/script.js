30f435a774293ac7e4891cc4da0bc4db6803ad4d
unknown <arthurguilherme2013@gmail.com>
2022-11-04 06:32:42 +0000

    Fim da revisão sobre Tipos de dados e Operadores Lógicos

---
+++ 2_TiposDeDados/10_Boolean_Operador_Lógico/js/script.js
@@ -0,0 +1,11 @@
+console.log(5 > 3 && 3 == 2);
+console.log(5 > 3 || 3 == 1);
+console.log("______");
+console.log(3 == 3 && "Arthur" == "Arthur");
+console.log("Arthur" == "Guilherme"|| false);
+console.log("______");
+console.log(!(true && true));
+console.log(!(!(true && true)));
+console.log("______");
+console.log(true && true);
+console.log(false || false);
---
+++ 2_TiposDeDados/11_Boolean_Operador_Ternario/js/script.js
@@ -0,0 +1,4 @@
+console.log(5 > 3 ? "É sim!" : "Não é!");
+console.log(5 < 3 ? "É sim!" : "Não é!");
+console.log(false ? "É verdadeiro!" : "Falso!");
+console.log(("Arthur" == "Arthur") ? "Hey, whatsapp!" : "Fault!");
