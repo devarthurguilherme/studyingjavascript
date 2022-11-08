8516247fae5cd34d7d6591b7886267e46445024e
unknown <arthurguilherme2013@gmail.com>
2022-11-08 07:33:21 +0000

    Estudo das Estruturas de Repetição: While

---
+++ 4_Estruturas_De_Programacao/11_elseIf/js/script.js
@@ -0,0 +1,10 @@
+let nome = "Arthur";
+let idade = 31
+
+if(nome != undefined && nome == "Guilherme") {
+    console.log("Esse nome já está definido!")
+} else if(nome == "Arthur" && nome.length > 5 && idade == 30){
+    console.log("O nome é Arthur")
+} else {
+    console.log("Não é Arthur");
+}
---
+++ 4_Estruturas_De_Programacao/12_while/js/script.js
@@ -0,0 +1,6 @@
+let x = 0;
+
+while( x <= 10) {
+    console.log(`X é = a ${x}`);
+    x = x + 1;
+}
