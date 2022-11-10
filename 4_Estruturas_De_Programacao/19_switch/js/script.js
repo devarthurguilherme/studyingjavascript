fd30977aae827470e2ae4fa93a174e2b6187aa19
unknown <arthurguilherme2013@gmail.com>
2022-11-10 07:06:46 +0000

    Estudando condicional 'Switch' e loop 'For'

---
+++ 4_Estruturas_De_Programacao/19_switch/js/script.js
@@ -0,0 +1,14 @@
+let nome = "uilherme";
+
+switch(nome) {
+    case "Arthur":
+        console.log("O nome é Arthur");
+        break;
+    case "Guilherme":
+        console.log("O nome é Guilherme");
+        break;
+    default:
+        console.log("Nenhum nome encontrado")
+}
+
+//hahas
