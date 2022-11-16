a1ac13bbc56552a70b97b696d500921cab2926f7
unknown <arthurguilherme2013@gmail.com>
2022-11-16 06:42:08 +0000

    Revisando Arrays: O que são?, Acessar elementos

---
+++ 8_Arrays_e_Objetos/1_array/js/script.js
@@ -0,0 +1,4 @@
+let arr = [1, 2, 3, 4, 5];
+let nomes = ["Arthur", "Thávia", "Léa"];
+
+console.log(arr, nomes);
---
+++ 8_Arrays_e_Objetos/2_mais_sobre_array/js/script.js
@@ -0,0 +1,5 @@
+let arr = [1, 2, 3, 4, 5];
+let nomes = ["Arthur", "Thávia", "Léa"];
+
+console.log(arr.length);
+console.log(arr[arr.length - 1]);
